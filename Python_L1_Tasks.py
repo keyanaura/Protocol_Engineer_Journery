8f385b6f41d55b59eed7307afe16438cf075f34a
Keyan <keyan.aura@gmail.com>
2025-12-15 22:19:30 -0600

    Commiting script prints Name and Age.

---
+++ Python_L1_Tasks.py
@@ -0,0 +1,9 @@
+def PrintValues(name, age):
+    print("name: ", name)
+    print("age: ", age)
+
+
+if __name__ == "__main__":
+    name = input("Enter Name: ")
+    age = input("Enter age: ")
+    PrintValues(name, age)
